fea412ad8785e1ac78729423dd21babbcf0fdc6c
NonokaM <c1201071@gmail.com>
2024-04-27 21:32:54 +0900

    feat: login func

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,43 @@
-export default function LogIn() {
+"use client";
+
+import React from "react";
+import { useEffect } from "react";
+
+import { redirect } from "next/navigation";
+import { signIn } from "next-auth/react";
+import { useSession } from "next-auth/react";
+
+const LoginPage = () => {
+  const { data: session, status } = useSession();
+  useEffect(() => {
+    // ログイン済みの場合はTOPページにリダイレクト
+    if (status === "authenticated") {
+      redirect("/");
+    }
+  }, [session, status]);
+
+  const handleLogin = (provider: string) => async (event: React.MouseEvent) => {
+    event.preventDefault();
+    const result = await signIn(provider);
+
+    // ログインに成功したらTOPページにリダイレクト
+    if (result) {
+      redirect("/");
+    }
+  };
+
   return (
-    <>login</>
+    <div>
+      <form>
+        <button
+          onClick={handleLogin("google")}
+          type="button"
+        >
+          Googleでログイン
+        </button>
+      </form>
+    </div>
   );
-}
+};
+
+export default LoginPage;
