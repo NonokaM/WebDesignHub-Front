188f38ed095a82df1d26b7b93c8d46848cab68eb
NonokaM <c1201071@gmail.com>
2024-04-30 01:05:26 +0900

    refactor: 不要なimport削除, debugモードOFF

--- src/utils/next-auth-options.ts
+++ src/utils/next-auth-options.ts
@@ -1,16 +1,11 @@
-import TwitterProvider from "next-auth/providers/twitter";
 import GoogleProvider from "next-auth/providers/google";
 
 import type { NextAuthOptions } from "next-auth";
 
 export const nextAuthOptions: NextAuthOptions = {
-  debug: true,
+  debug: false,
   session: { strategy: "jwt" },
   providers: [
-    TwitterProvider({
-      clientId: process.env.TWITTER_CLIENT_ID!,
-      clientSecret: process.env.TWITTER_CLIENT_SECRET!,
-    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
